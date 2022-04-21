2c662d7849e33963907ac44787067ad744d1191c
RPL-NeliaDane <neliadane17@gmail.com>
2022-04-21 09:04:25 +0700

    Contoh Penerapan Intent 1

---
+++ app/src/main/java/id/ac/polbeng/nelia/myapplicationintent/MainActivity.kt
@@ -0,0 +1,38 @@
+package id.ac.polbeng.nelia.myapplicationintent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.logging.Logger
+
+class MainActivity : AppCompatActivity() {
+    val Log = Logger.getLogger(MainActivity::class.java.name)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.info("oncreate")
+        btnSA.setOnClickListener {
+            val m_intent = Intent (this@MainActivity,
+                SecondActivity::class.java)
+            startActivity(m_intent)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.info("onPause")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.info("onRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.info("onResume")
+    }
+}
---
+++ app/src/main/java/id/ac/polbeng/nelia/myapplicationintent/SecondActivity.kt
@@ -0,0 +1,30 @@
+package id.ac.polbeng.nelia.myapplicationintent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+import java.util.logging.Logger
+
+class SecondActivity : AppCompatActivity() {
+    var Log = Logger.getLogger(SecondActivity::class.java.name)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        Log.info("onCreate")
+        btnExitActivity.setOnClickListener {
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.info("onStart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.info("onStop")
+    }
+}
